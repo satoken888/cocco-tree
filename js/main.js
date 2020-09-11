7741c38b8b2392985f9f2cbeb771907331550eee
s1260177_Yuki Sakurai <52778944+s1260177@users.noreply.github.com>
2020-09-11 20:57:51 +0900

    MENUの表記変更 ボタンで表示非表示の切り替え ドリンクバーへのsmoothscroll

--- js/main.js
+++ js/main.js
@@ -32,5 +32,19 @@ jQuery(document).ready(function( $ ) {
       verticalOffset: 40
     });
 
+		//Menu 表示非表示切り替え
+				$(function(){
+				  $('.section').hide();
+
+				  $('.secList').on('click',function(){
+				    // クリックした要素の ID と違うクラス名のセクションを非表示
+				    $('.section').not($('.'+$(this).attr('id'))).hide();
+				    // クリックした要素の ID と同じクラスのセクションを表示
+				   // $('.'+$(this).attr('id')).show();
+
+				    // toggle にすると、同じボタンを 2 回押すと非表示になる
+				    $('.'+$(this).attr('id')).toggle();
+				  });
+				});
 
 });
