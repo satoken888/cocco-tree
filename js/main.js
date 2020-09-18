c9448716aa3dc60a2a1ee4f7ba75f44a95238497
Tarun Sreepada <41629493+rageristic@users.noreply.github.com>
2020-09-18 20:30:40 +0900

    Shift parallax, menu, access locations

--- js/main.js
+++ js/main.js
@@ -1,50 +1,50 @@
-jQuery(document).ready(function( $ ) {
+jQuery(document).ready(function($) {
 
-		// Menu settings
-		$('#menuToggle, .menu-close').on('click', function(){
-			$('#menuToggle').toggleClass('active');
-			$('body').toggleClass('body-push-toleft');
-			$('#theMenu').toggleClass('menu-open');
-		});
+    // Menu settings
+    $('#menuToggle, .menu-close').on('click', function() {
+        $('#menuToggle').toggleClass('active');
+        $('body').toggleClass('body-push-toleft');
+        $('#theMenu').toggleClass('menu-open');
+    });
 
-		// Smooth scroll for the menu and links with .scrollto classes
-	  $('.smoothscroll').on('click', function(e) {
-	    e.preventDefault();
-	    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-	      var target = $(this.hash);
-	      if (target.length) {
+    // Smooth scroll for the menu and links with .scrollto classes
+    $('.smoothscroll').on('click', function(e) {
+        e.preventDefault();
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            if (target.length) {
 
-	        $('html, body').animate({
-	          scrollTop: target.offset().top
-	        }, 1500, 'easeInOutExpo');
-	      }
-	    }
-			$('body').toggleClass('body-push-toleft');
-			$('#theMenu').toggleClass('menu-open');
-	  });
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1500, 'easeInOutExpo');
+            }
+        }
+        $('body').toggleClass('body-push-toleft');
+        $('#theMenu').toggleClass('menu-open');
+    });
 
     // Init Fancybox
     jQuery(".fancybox").fancybox();
 
     // Init Stellar
     $.stellar({
-      horizontalScrolling: false,
-      verticalOffset: 40
+        horizontalScrolling: false,
+        verticalOffset: 40
     });
 
-		//Menu 表示非表示切り替え
-				$(function(){
-				  $('.section').hide();
+    //Menu 表示非表示切り替え
+    $(function() {
+        $('.section').hide();
 
-				  $('.secList').on('click',function(){
-				    // クリックした要素の ID と違うクラス名のセクションを非表示
-				    $('.section').not($('.'+$(this).attr('id'))).hide();
-				    // クリックした要素の ID と同じクラスのセクションを表示
-				   // $('.'+$(this).attr('id')).show();
+        $('.secList').on('click', function() {
+            // クリックした要素の ID と違うクラス名のセクションを非表示
+            $('.section').not($('.' + $(this).attr('id'))).hide();
+            // クリックした要素の ID と同じクラスのセクションを表示
+            // $('.'+$(this).attr('id')).show();
 
-				    // toggle にすると、同じボタンを 2 回押すと非表示になる
-				    $('.'+$(this).attr('id')).toggle();
-				  });
-				});
+            // toggle にすると、同じボタンを 2 回押すと非表示になる
+            $('.' + $(this).attr('id')).toggle();
+        });
+    });
 
 });
